3b47999cf2b374f7c1818f25d32864e1dbc0f654
1jhilik <rokshan.jhilik2016@gmail.com>
2022-08-02 20:25:32 +0800

    account added

--- index.js
+++ index.js
@@ -41,18 +41,18 @@ async function run() {
     })
 
 
-    // // payment stripe account api 
-    // app.post('/create-payment-intent', async (req, res) => {
-    //   const service = req.body;
-    //   const price = service.price;
-    //   const amount = price * 100;
-    //   const paymentIntent = await stripe.paymentIntents.create({
-    //     amount: amount,
-    //     currency: 'usd',
-    //     payment_method_types: ['card']
-    //   });
-    //   res.send({ clientSecret: paymentIntent.client_secret })
-    // });
+    // payment stripe account api 
+    app.post('/create-payment-intent', async (req, res) => {
+      const service = req.body;
+      const price = service.price;
+      const amount = price * 100;
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types: ['card']
+      });
+      res.send({ clientSecret: paymentIntent.client_secret })
+    });
 
 
 
